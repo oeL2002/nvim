a1458d50053780fddd8d1959af00bf1bdc3965a5
Leo Jöhr <leo.joehr@gmail.com>
2026-05-03 19:39:24 +0200

    refactor to new syntax

--- init.lua
+++ init.lua
@@ -98,7 +98,9 @@ vim.diagnostic.config {
   virtual_lines = false, -- Text shows up underneath the line, with virtual lines
 
   -- Auto open the float, so you can easily read the errors when jumping with `[d` and `]d`
-  jump = { float = true },
+  jump = {
+    on_jump = function(_, _) vim.diagnostic.open_float() end,
+  },
 }
 
 vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostic [Q]uickfix list' })
